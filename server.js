2d5ed6b8612c4e6928bac67a8e68e756483bca13
nau20 <95400353+nau20@users.noreply.github.com>
2023-06-08 14:13:22 +0900

    add flag language

--- server.js
+++ server.js
@@ -2,13 +2,16 @@ const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const i18n = require("./i18N");
 
 // set the view engine to ejs
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
+// app.use(i18n);
 
 app.get('/', (req, res) => {
+    // console.log(res.__('Hello i18n'));
     res.render('index');
 });
 app.get('/resume', (req, res) => {
